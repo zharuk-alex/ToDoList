b74e6021fb9c9f7b28239aa8649c2b0de9f0102d
Zharuk Alexander <zzharuk@gmail.com>
2019-08-05 00:38:23 +0300

    TodoList Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+
+
+interface NewItem  {
+  id: number,
+  status: boolean,
+  text: string,
+  title: string
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'ToDoList';
+  
+  newTitle="";
+  newText="";
+  faSquare = faSquare;
+  faCheckSquare = faCheckSquare;
+  
+
+
+  addNew() {
+    const newItem:NewItem = {
+      id: this.todosArray.length+1,
+      status: false,
+      text: this.newText,
+      title: this.newTitle
+    }
+
+    this.todosArray.unshift(newItem);
+    this.newText = "";
+    this.newTitle = "";
+  }
+  
+  delItem(text) {
+    const index = this.todosArray.findIndex((el) => el.text === text);
+    this.todosArray.splice(index,1);
+  }
+
+  show(){
+    console.log(this.todosArray);
+  }
+
+  todosArray = [];
+
+  getData() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(todos => todos.map((td) => {
+      return {
+        id: td.id,
+        status: td.completed,
+        text: td.title,
+        title: td.reqtitle ? td.reqtitle : `Untitled ${td.id}`
+      }
+    }))
+
+    .then(todos => {
+      todos.length = 10;
+      return todos;
+    })
+    .then(todos => {
+      return this.todosArray = todos;
+    })
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    FontAwesomeModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
